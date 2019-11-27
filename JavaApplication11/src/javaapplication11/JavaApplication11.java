a5c53abe18f7bcaf10b5183a2f6252c0fef85384
Kamil <kamil6217@gmail.com>
2019-11-27 10:54:55 +0100

    finalna wersja programu

--- JavaApplication11/src/javaapplication11/JavaApplication11.java
+++ JavaApplication11/src/javaapplication11/JavaApplication11.java
@@ -5,17 +5,48 @@
  */
 package javaapplication11;
 
-/**
- *
- * @author student
- */
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+
 public class JavaApplication11 {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        int as;
+   static int liczslowa(String string)  
+        {  
+          int count=0;
+  
+      
+            char ch[]= new char[string.length()];     
+            for(int i=0;i<string.length();i++)  
+            {  
+                ch[i]= string.charAt(i);  
+                if( ((i>0)&&(ch[i]!=' ')&&(ch[i-1]==' ')) || ((ch[0]!=' ')&&(i==0)) || (ch[i-1]==','))
+                    count++;  
+            }  
+            
+            
+            return count;  
+        }  
+    public static void main(String[] args) throws FileNotFoundException {
+        Scanner scan=new Scanner(System.in);
+        String string=scan.nextLine();
+        int licz=liczslowa(string);
+        if(licz==0){
+        System.out.println("help");
+                } else
+        System.out.println(liczslowa(string) + " slow."); 
+        
+         File file = new File("file.txt");
+            try(Scanner sc = new Scanner(new FileInputStream(file))){
+            int count=0;
+                while(sc.hasNext()){
+                    sc.next();
+                    count++;
+                                    }
+        System.out.println("Ilosc slow: " + count);
+}
     }
     
 }
